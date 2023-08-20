94a7233a88e0f14792d76d4303dc9cf707207483
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-08-20 18:40:17 +0530

    Next Auth and prisma setup done

---
+++ src/app/api/(auth)/[...nextauth]/route.ts
@@ -0,0 +1,65 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import { db } from "@/lib/db";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import bcrypt from "bcrypt";
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(db),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "john@doe.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        // check to see if email and password is there
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Please enter an email and password");
+        }
+
+        // check to see if user exists
+        const user = await db.user.findUnique({
+          where: {
+            email: credentials?.email,
+          },
+        });
+
+        // if no user was found
+        if (!user || !user?.hashedPassword) {
+          throw new Error("No user found");
+        }
+
+        // check to see if password matches
+        const passwordMatch = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        // if password does not match
+        if (!passwordMatch) {
+          throw new Error("Incorrect password");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  debug: process.env.NODE_ENV === "development",
+};
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
---
+++ src/app/register/route.ts
@@ -0,0 +1,34 @@
+import bcrypt from "bcrypt";
+import { db } from "../../lib/db";
+import { NextResponse } from "next/server";
+
+export async function POST(request: { json: () => any }) {
+  const body = await request.json();
+  const { name, email, password } = body;
+
+  if (!name || !email || !password) {
+    return new NextResponse("Missing Fields", { status: 400 });
+  }
+
+  const exist = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (exist) {
+    throw new Error("Email already exists");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await db.user.create({
+    data: {
+      name,
+      email,
+      hashedPassword,
+    },
+  });
+
+  return NextResponse.json(user);
+}
