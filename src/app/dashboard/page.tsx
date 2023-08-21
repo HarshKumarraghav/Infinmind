6b3e5ee4a89fceaf37c0288766a3f214a17f31f7
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-08-21 08:39:36 +0530

    Main header work initiated

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,19 @@
 "use client";
+import Header from "@/components/Header/Header";
 import { Button } from "@/components/ui/button";
 import { signOut } from "next-auth/react";
 const page = () => {
   return (
-    <Button
-      onClick={() => {
-        signOut();
-      }}
-    >
-      Sign Out
-    </Button>
+    <div className="w-screen h-screen">
+      <Header />
+      <Button
+        onClick={() => {
+          signOut();
+        }}
+      >
+        Sign Out
+      </Button>
+    </div>
   );
 };
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
 import ThemeSwitcher from "../Theme/ThemeSwitcher";
+import UserNav from "./UserNav";
 
-const Navbar = () => {
+const Header = () => {
   const Router = useRouter();
   return (
-    <header className="sticky top-0 z-40 w-full border-b border-slate-900/10 dark:border-slate-300/10">
-      <div className="container mx-auto flex flex-row justify-between items-center py-4 px-2">
+    <div className="w-full h-14 border-b border-slate-900/10 dark:border-slate-300/10">
+      <div className="container mx-auto flex flex-row justify-between items-center h-full px-2">
         <div className="flex items-center justify-center md:mb-0 md:mr-4 gap-x-2">
           <Image
             width={20}
@@ -26,16 +27,11 @@ const Navbar = () => {
         </div>
         <div className="flex items-center space-x-4">
           <ThemeSwitcher />
-          <button
-            onClick={() => Router.push("/sign-in")}
-            className="bg-primary py-2 px-4 rounded-full text-[10px]  md:text-sm text-white focus:outline-none hover__button"
-          >
-            Try Now
-          </button>
+          <UserNav />
         </div>
       </div>
-    </header>
+    </div>
   );
 };
 
-export default Navbar;
+export default Header;
