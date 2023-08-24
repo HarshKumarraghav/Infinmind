e0455e1106bf87af75f5f4fd05b0d45055b69729
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-08-24 06:29:35 +0530

    Favicon and TopProgressbar option sepreated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,10 @@ import AuthContextProvider from "@/Providers/AuthProvider";
 import ThemeProviders from "@/Providers/ThemeProvider";
 import NextTopLoader from "nextjs-toploader";
 import { ModalProvider } from "@/Providers/ModalProvider";
+import { TopLoaderOptions } from "../../utils/constants";
 export const metadata: Metadata = {
   title: "InfiniMind.ai",
   description: "InfiniMind.ai is a platform for creating and sharing ideas.",
-  icons: {
-    icon: "/logo/logo.png",
-  },
 };
 export default function RootLayout({
   children,
@@ -19,18 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo/logo.png" />
+      </head>
       <body suppressHydrationWarning={true}>
-        <NextTopLoader
-          color="#2563eb"
-          initialPosition={0.08}
-          crawlSpeed={200}
-          height={5}
-          crawl={true}
-          showSpinner={false}
-          easing="ease"
-          speed={200}
-          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
-        />
+        <NextTopLoader {...TopLoaderOptions} />
         <ThemeProviders>
           <ModalProvider />
           <AuthContextProvider>
--- utils/constants.ts
+++ utils/constants.ts
@@ -7,6 +7,17 @@ import {
 import { FaCode } from "react-icons/fa";
 
 export const MAX_FREE_COUNTS = 5;
+export const TopLoaderOptions = {
+  color: "#2563eb",
+  initialPosition: 0.08,
+  crawlSpeed: 200,
+  height: 5,
+  crawl: true,
+  showSpinner: false,
+  easing: "ease",
+  speed: 200,
+  shadow: "0 0 10px #2299DD,0 0 5px #2299DD",
+};
 export const ToolsData = [
   {
     id: 1,
